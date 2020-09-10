63e4bfaa76e75f807c987f60114681362394240b
Khs98 <62372634+Khs98@users.noreply.github.com>
2020-09-10 17:32:11 +0900

    Add files via upload

---
+++ aa2-13-rpt02/start/hello_function.js
@@ -0,0 +1,6 @@
+function hello(what) {
+  console.log("Hello " + what + "!");
+}
+
+hello("aa13");
+hello("aa13,김현식");
---
+++ aa2-13-rpt02/start/hello_my_module.js
@@ -0,0 +1,4 @@
+var olleh = require("./start/hello_module.js");
+
+olleh("aa13");
+olleh("Node");
